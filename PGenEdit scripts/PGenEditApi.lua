46f6b57db23ac6723a5ce302de47301f7397e3c2
Eksekk <eksekk3@gmail.com>
2023-11-27 12:44:20 +0100

    Lua debug api progress

--- PGenEdit scripts/PGenEditApi.lua
+++ PGenEdit scripts/PGenEditApi.lua
@@ -28,6 +28,7 @@ local sCantConvertToNumber = "'%s': Parameter %d (%q) is not a number and can't 
 local sCantConvertToBoolean = "'%s': Parameter %d (%q) is not a boolean and can't be converted to it"
 local sCantConvertToString = "'%s': Parameter %d (%q) is not a string and can't be converted to it"
 local sCantConvertToTable = "'%s': Parameter %d (%q) is not a table and can't be converted to it"
+local sNotAnObject = "Argument %d (%q) is not an object [userdata]"
 local function validateOrConvertParameter(value, typ, paramIndex, funcName)
 	local conv = pgenedit.debug.attemptTypeConversion
 	if integerTypeRanges[typ] then
@@ -110,27 +111,64 @@ local function validateOrConvertParameter(value, typ, paramIndex, funcName)
 		error(format(sCantConvertToTable, funcName, paramIndex, value), 3)
 	elseif typ == "nil" then -- shouldn't ever happen
 		error(format("'%s': Parameter %d requested type is 'nil'", funcName, paramIndex), 3)
+	elseif typ == "object" then
+		if type(value) ~= "userdata" or not getmetatable(value) or not getmetatable(value).__className then
+			error(format(sNotAnObject, funcName, paramIndex, value), 3)
+		end
 	end
 end
 
-local function funcWrapper(f, argTypes, retTypes)
+-- if className is nil, then it's treated as a global/static function
+local function funcWrapper(className, funcName)
+	local paramInfos = api.getMethodArgumentInfos(className, funcName)
+	-- single parameter table has following fields:
+	-- name - as it appears in C++ code
+	-- type - for objects of any kind it's "object", specific type name will be stored in className
+	-- className - only for class objects, name of the class
+	-- hasDefaultValue - true if parameter has default value
+	-- defaultValue - default value, if hasDefaultValue is true
+	-- isConst
+	-- isReference
+	-- isPointer
+
+	-- isStatic
+	-- isEnum
+	-- isClass
+	-- isFunction
+	-- isMethod
+	-- isField
+	-- isVariable
+	-- isGlobal
+	-- isStaticMethod
+	-- isStaticField
+	local argTypes = api.getMethodArgumentTypes(className, funcName)
+	local argCount, defCount = api.getMethodArgumentCount(className, funcName), api.getDefaultArgumentCount(className, funcName)
+	assert(argCount >= defCount, format("'%s': Argument count (%d) is less than default argument count (%d)", funcName, argCount, defCount))
+	assert(#argTypes == argCount, format("'%s': Argument type count (%d) is not equal to argument count (%d)", funcName, #argTypes, argCount))
 	return function(...)
 		local args = {...}
-		for i, typ in ipairs(argTypes) do
-			if typ == "object" then
-				if type(args[i]) ~= "table" or not getmetatable(args[i]) or not getmetatable(args[i]).__className then
-					error(format("Argument %d is not an object", i), 2)
-				end
-			elseif 
+		if #args < argCount - defCount then
+			error(format("'%s': Not enough arguments (expected at least %d, got %d)", funcName, argCount - defCount, #args), 2)
+		elseif #args > argCount then
+			error(format("'%s': Too many arguments (expected at most %d, got %d)", funcName, argCount, #args), 2)
+		end
+		for i = className and 2 or 1, #argTypes do -- if class method, skip first argument, it's processed later
+			local typ = argTypes[i]
+			args[i] = validateOrConvertParameter(args[i], typ, i, funcName)
+		end
+		if className then
+			local this = args[1]
+			if not api.classExists(className) then
+				error(format("Class %q does not exist", className), 2)
+			elseif type(this) ~= "userdata" then
+				error(format("Invalid object instance (first parameter), received %q", this), 2)
+			elseif not getmetatable(this) then
+				error(format("Object instance is not a valid object (no metatable)", this), 2)
+			elseif getmetatable(this).__className ~= className then
+				error(format("Object instance is of invalid class (expected %q, got %q)", this, className, getmetatable(this).__className), 2)
 			end
 		end
-		local ret = {f(unpack(args))}
-		for i = 1, #ret do
-			if type(ret[i]) == "userdata" then
-				ret[i] = api.makeObject(ret[i])
-			end
-		end
-		return unpack(ret)
+		return api.callMethod(className, funcName, #args, unpack(args))
 	end
 end
 
@@ -139,23 +177,32 @@ local function createObjectMetatable(className)
 	local mt = {}
 	mt.__className = className
 	function mt.__index(obj, key)
-		assert(getmetatable(obj).__className == className, format("Received object of type %q, expected %q", getmetatable(obj).__className, className))
+		if getmetatable(obj).__className ~= className then
+			error(format("Received object of type %q, expected %q", getmetatable(obj).__className, className), 2)
+		end
 		if api.isMethodOfClass(className, key) then
-			local f = api.makeMethodOfClass(className, key)
+			local f = funcWrapper(className, key)
 			rawset(obj, key, f)
 			return f
 		elseif api.isFieldOfClass(className, key) then
 			return api.getFieldOfClass(obj, className, key)
 		else
-			error(format("Attempt to access unknown field %q of class %q", key, className))
+			error(format("Attempt to access unknown field %q of class %q", key, className), 2)
 		end
 	end
 	function mt.__newindex(obj, key, value)
-		assert(getmetatable(obj).__className == className, format("Received object of type %q, expected %q", getmetatable(obj).__className, className))
-		assert(not api.isMethodOfClass(className, key), format("Attempt to set method %q of class %q", key, className))
+		if getmetatable(obj).__className ~= className then
+			error(format("Received object of type %q, expected %q", getmetatable(obj).__className, className), 2)
+		end
+		if api.isMethodOfClass(className, key) then
+			error(format("Attempt to set method %q of class %q", key, className), 2)
+		elseif not api.isFieldOfClass(className, key) then
+			error(format("Attempt to set unknown field %q of class %q", key, className), 2)
+		end
 		api.setFieldOfClass(obj, className, key, value)
 	end
 	function mt.__new(...)
+		-- TODO: find constructor matching parameters (count, types and possible conversions between types) and default arguments, if no matching constructor found, throw an error
 		local obj = api.createObject(className, ...)
 		setmetatable(obj, mt)
 		return obj
@@ -163,16 +210,25 @@ local function createObjectMetatable(className)
 	function mt.__gc(obj)
 		api.destroyObject(obj)
 	end
-	function mt.__copy(...)
-		local obj = api.copyObject(...)
+	function mt.__copy(this)
+		local obj = api.copyObject(this)
 		setmetatable(obj, mt)
 		return obj
 	end
+	-- TODO: dumpAllProperties (including functions shown clearly as such)
+	-- TODO: readonly properties
 	return mt
 end
 
+-- call stack: user code -> pgenedit.class index -> makeClass -> createObjectMetatable
+-- !!! if called from __index or __newindex of metatable, then stack is: user code -> our function (max 2 depth)
+
 local function makeClass(name, ...)
-	assert(api.classExists(name), format("Class %q does not exist", name))
+	if name == nil then
+		error("Class name is nil", 3)
+	elseif not api.classExists(name) then
+		error(format("Class %q does not exist", name), 3)
+	end
 	local classMT = {}
 	local objMT = createObjectMetatable(name)
 	classMT.objectMetatable = objMT
@@ -185,33 +241,36 @@ local function makeClass(name, ...)
 	-- static variables
 	function classMT.__index(t, str)
 		if api.isStaticMethodOfClass(name, str) then
-			local f = api.makeStaticMethodOfClass(name, str)
+			local f = funcWrapper(name, str)
 			rawset(t, str, f)
 			return f
 		elseif api.isStaticFieldOfClass(name, str) then
 			return api.getStaticFieldOfClass(name, str)
 		else
-			error(format("Attempt to access unknown static field %q of class %q", str, name))
+			error(format("Attempt to access unknown static field %q of class %q", str, name), 2)
 		end
 	end
 	function classMT.__newindex(t, key, value)
-		assert(not api.isStaticMethodOfClass(name, key), format("Attempt to set static method %q of class %q", key, name))
+		assert(not api.isStaticMethodOfClass(name, key), format("Attempt to set static method %q of class %q", key, name), 2)
 		if api.isStaticFieldOfClass(name, key) then
 			api.setStaticFieldOfClass(name, key, value)
 		else
-			error(format("Attempt to set unknown static field %q of class %q", key, name))
+			error(format("Attempt to set unknown static field %q of class %q", key, name), 2)
 		end
 	end
-	return classMT
+	return setmetatable({}, classMT)
 end
 
 local class = {}
 pgenedit.class = class
 local mt = {}
 function mt.__index(t, key) -- if __index fires, this means that class is not yet created
-	local classMT = makeClass(key)
-	rawset(t, key, classMT)
-	return classMT
+	local cls = makeClass(key)
+	rawset(t, key, cls)
+	return cls
+end
+function mt.__newindex(t, key, value)
+	error(format("Attempt to set class %q", key), 2)
 end
 setmetatable(class, mt)
 -- pgenedit.class.wxWindow.new("test", nil, 0, 0, 0, 0)
@@ -223,7 +282,7 @@ local mt = {}
 setmetatable(global, mt)
 function mt.__index(t, key)
 	if api.isGlobalFunction(key) then
-		local f = api.makeGlobalFunction(key)
+		local f = funcWrapper(nil, key)
 		rawset(t, key, f)
 		return f
 	elseif api.isGlobalVariable(key) then
